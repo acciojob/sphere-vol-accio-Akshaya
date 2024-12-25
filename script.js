48334c53e44a22a3e0df6a7b9c3e74f80e51a89d
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-25 06:37:56 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	const radiusInput = document.getElementById('radius').value;
+	const radius = parseFloat(radiusInput);
+
+	if(isNaN(radius) || radius < 0){
+		document.getElementById('radius').value=NaN;
+		return;
+	}
+	const volume = (4/3)*Math.Pi * Math.pow(radius,3);
+	document.getElementById('volume').value = volume.toFixed(4);
   
 } 
 
